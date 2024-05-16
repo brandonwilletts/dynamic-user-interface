041ff9ec5a650818d7d854567e4f2794941eb5f3
Brandon Willetts”git config --global user.email “brandonwilletts@gmail.com”git config --global user.name Brandon <brandonwilletts@gmail.com>
2024-05-15 20:00:54 -0700

    Import images in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import dropdownMenus from "./dropdown-menus";
 import carousel from "./image-carousel";
 import "./style.css";
+import canucks from "./img/canucks.jpeg";
+import joshua from "./img/joshua.jpeg";
+import linden from "./img/linden.jpeg";
+import silovs from "./img/silovs.jpeg";
 
 dropdownMenus();
 carousel();
